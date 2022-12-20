f9b95cfb84e0c48b09422e4c7af5d8a4caa6e9d8
Marco 9726 <marcosarra9@gmail.com>
2022-12-20 15:27:57 +0100

    visualizzo la lista a schermo

--- js/main.js
+++ js/main.js
@@ -6,8 +6,32 @@ const {
 createApp({
     data(){
         return {
-           
-
+            list:[
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Portare fuori il cane',
+                    done: false
+                },
+                {
+                    text: 'Lavare la macchina',
+                    done: true
+                },
+                {
+                    text: 'Passare in banca',
+                    done: false
+                },
+                {
+                    text: 'Sistemare il giardino',
+                    done: true
+                },
+                {
+                    text: 'Fare l\'esercizio',
+                    done: false
+                }
+            ]
         }
     },
     methods: {
